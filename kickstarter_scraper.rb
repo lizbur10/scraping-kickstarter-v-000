929142d69ae04813e205b07a5db1162cf97391b6
Liz Burton <liz.burton147@gmail.com>
2017-12-07 16:52:52 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,6 +9,8 @@ def create_project_hash
 # title: project.css("h2.bbcard_name strong a").text
 # images link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
+# percent funded: project.css("ul.project-stats li.first.funded strong").text
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end

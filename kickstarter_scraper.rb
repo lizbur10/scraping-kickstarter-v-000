2b0ac3229a83c3a623cae3033fc0b5f04d4853df
Liz Burton <liz.burton147@gmail.com>
2017-12-07 17:03:37 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,6 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-  kickstarter.css("li.project.grid_4").first
   # projects = {}
   #
   # kickstarter.css("li.project.grid_4").each do | project |
